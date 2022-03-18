58f1443eace10fe0e5b933e0fef569b12b01bdd5
knyttali <amanda_andrae@hotmail.com>
2022-03-18 21:49:06 +0100

    update score

--- script.js
+++ script.js
@@ -54,4 +54,24 @@ function addWordToDOM(){
     word.innerHTML = randomWord;
 }
 
+// Update score
+function updateScore(){
+    score++;
+    scoreEl.innerHTML = score;
+}
+
 addWordToDOM();
+
+// Event listeners
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+
+
+    if(insertedText === randomWord){
+        addWordToDOM();
+        updateScore();
+
+        // Clear
+        e.target.value = '';
+    }
+})
